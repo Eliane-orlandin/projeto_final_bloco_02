bac16cfe3c060f7f0e8aaedce5de449b35595255
Eliane Orlandin <liorlandin33@gmail.com>
2026-06-08 11:24:55 -0300

    feat: adiciona entidade Categoria ao model

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,41 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	// Atributos
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O Atributo Nome é obrigatório")
+	private String nome;
+
+	// Getters e Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
