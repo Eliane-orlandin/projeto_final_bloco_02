b8ee2b325bcb87352b6ccb5bdc0ec8eee93ca9a1
Eliane Orlandin <liorlandin33@gmail.com>
2026-06-08 12:19:04 -0300

    feat: adiciona entidade Produto ao model

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,112 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	// Atributos: id, nome, descricao, preco, quantidade, localdate
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	@Size(min = 5, max = 500, message = "O atributo descrição deve conter no mínimo 05 e no máximo 500 caracteres")
+	private String descricao;
+
+	@Positive
+	@NotNull(message = "O atributo preço é obrigatório")
+	private BigDecimal preco;
+
+	@NotNull(message = "O atributo quantidade é obrigatório!")
+	@Min(0)
+	private Integer quantidade;
+
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	// Relacionamento com a tabela Categoria
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Categoria categoria;
+
+	// Getters e Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
